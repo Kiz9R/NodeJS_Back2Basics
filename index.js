f7a28b7249e24bb929494eddb18be7a11d23bc56
Ritankar Debnath <ritankarxkiz9r@gmail.com>
2023-09-22 22:27:16 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// const fs = require("fs");
+const fs = require("fs");
 
-// const t1 = performance.now();
-// fs.readFile("./Assets/doc.txt", "utf-8", (err, data) => {
-//   console.log(data);
-//   console.log(t1);
-// });
+const t1 = performance.now();
+fs.readFile("./Assets/doc.txt", "utf-8", (err, data) => {
+  console.log(data);
+  console.log(t1);
+});
 
-// const sum = (a, b) => console.log(a + b);
-// const t2 = performance.now();
-// console.log(t2);
-// sum(6, 8);
+const sum = (a, b) => console.log(a + b);
+const t2 = performance.now();
+console.log(t2);
+sum(6, 8);
 
-// console.log(t1 - t2);
+console.log(t1 - t2);
