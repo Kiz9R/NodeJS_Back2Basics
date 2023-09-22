c26f294c1ac0d853ac5c96663017ee382d961814
Ritankar Debnath <ritankarxkiz9r@gmail.com>
2023-09-22 21:11:51 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// const fs = require("fs");
+
+// const t1 = performance.now();
+// fs.readFile("./Assets/doc.txt", "utf-8", (err, data) => {
+//   console.log(data);
+//   console.log(t1);
+// });
+
+// const sum = (a, b) => console.log(a + b);
+// const t2 = performance.now();
+// console.log(t2);
+// sum(6, 8);
+
+// console.log(t1 - t2);
